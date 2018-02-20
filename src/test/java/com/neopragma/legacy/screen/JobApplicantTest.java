25fe4f404f5297acadc66c1614698327b3afe183
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-19 22:56:30 -0500

    Move tests that hit an outside service into new package

--- src/test/java/com/neopragma/legacy/screen/JobApplicantTest.java
+++ src/test/java/com/neopragma/legacy/screen/JobApplicantTest.java
@@ -1,14 +1,9 @@
 package com.neopragma.legacy.screen;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import com.neopragma.legacy.screen.JobApplicant;
 
 /**
  * Automated unit checks for the base version of the JobApplicant application.
@@ -177,20 +172,6 @@ public class JobApplicantTest {
 	public void itRejectsSsn219099999() {
 		jobApplicant.setSsn("219099999");
 		assertEquals(4, jobApplicant.validateSsn());
-	}
-
-	@Test
-	public void itFindsAddisonTexasBy5DigitZipCode() throws URISyntaxException, IOException {
-		jobApplicant.setZipCode("75001");
-		assertEquals("Addison", jobApplicant.getCity());
-		assertEquals("TX", jobApplicant.getState());
-	}
-
-	@Test
-	public void itFindsMaranaArizonaBy9DigitZipCode() throws URISyntaxException, IOException {
-		jobApplicant.setZipCode("856585578");
-		assertEquals("Marana", jobApplicant.getCity());
-		assertEquals("AZ", jobApplicant.getState());
 	}
 
 }
