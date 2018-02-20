a99ebb1e472935dc760574156a922b6111a3a42e
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-20 00:13:16 -0500

    Move guts of lookupCityAndStateFromZip() into its own class

---
+++ src/test/java/com/neopragma/legacy/screen/integration/CityStateLookupTest.java
@@ -0,0 +1,30 @@
+package com.neopragma.legacy.screen.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+
+import com.neopragma.legacy.screen.CityStateLookup;
+
+public class CityStateLookupTest {
+
+	@Test
+	public void doesLookupCity() throws ClientProtocolException, URISyntaxException, IOException  {
+		CityStateLookup lookup = new CityStateLookup();
+		
+		lookup.lookupCityAndState("02134");
+		assertEquals("Allston", lookup.getCity());
+	}
+	
+	@Test
+	public void doesLookupState() throws ClientProtocolException, URISyntaxException, IOException  {
+		CityStateLookup lookup = new CityStateLookup();
+		
+		lookup.lookupCityAndState("02134");
+		assertEquals("MA", lookup.getState());
+	}
+}
