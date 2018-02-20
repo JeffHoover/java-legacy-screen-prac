c2b010d8e80143072694f8a5ddb30f10e09fd6cf
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-20 00:13:16 -0500

    Move guts of lookupCityAndStateFromZip() into its own class

---
+++ src/main/java/com/neopragma/legacy/screen/CityStateLookup.java
@@ -0,0 +1,75 @@
+package com.neopragma.legacy.screen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class CityStateLookup {
+
+	private String city;
+	private String state;
+
+	public void lookupCityAndState(String zipCode) throws URISyntaxException, IOException {
+
+		// Use a service to look up the city and state based on zip code.
+		// Save the returned city and state if content length is greater than zero.
+		URI uri = new URIBuilder()
+            .setScheme("http")
+            .setHost("www.zip-codes.com")
+            .setPath("/search.asp")
+            .setParameter("fld-zip", zipCode)
+            .setParameter("selectTab", "0")
+            .setParameter("srch-type", "city")
+            .build();
+        HttpGet request = new HttpGet(uri);
+
+        // TODO - Add dependency injection and mock CloseableHttpClient and CloseableHttpResponse
+        // so that we can test what happens should the lookup service fail
+
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            CloseableHttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+              	BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+           		StringBuilder result = new StringBuilder();
+           		String line = "";
+           		while ((line = rd.readLine()) != null) {
+           			result.append(line);
+       		    }
+                int metaOffset = result.indexOf("<meta ");
+                int contentOffset = result.indexOf(" content=\"Zip Code ", metaOffset);
+                contentOffset += 19;
+                contentOffset = result.indexOf(" - ", contentOffset);
+                contentOffset += 3;
+                int stateOffset = result.indexOf(" ", contentOffset);
+                city = result.substring(contentOffset, stateOffset);
+                stateOffset += 1;
+                state = result.substring(stateOffset, stateOffset+2);
+            } else {
+            	city = ""; // TODO - Not covered by a test - see above
+            	state = "";
+            }
+
+        }
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	
+}
--- src/main/java/com/neopragma/legacy/screen/JobApplicant.java
+++ src/main/java/com/neopragma/legacy/screen/JobApplicant.java
@@ -1,18 +1,8 @@
 package com.neopragma.legacy.screen;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Scanner;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 /**
  * Job applicant class.
@@ -109,51 +99,13 @@ public class JobApplicant {
 	}
 
 	public void lookupCityAndStateFromZip(String zipCode) throws URISyntaxException, IOException {
-		// Use a service to look up the city and state based on zip code.
-		// Save the returned city and state if content length is greater than zero.
-		URI uri = new URIBuilder()
-            .setScheme("http")
-            .setHost("www.zip-codes.com")
-            .setPath("/search.asp")
-            .setParameter("fld-zip", zipCode)
-            .setParameter("selectTab", "0")
-            .setParameter("srch-type", "city")
-            .build();
-        HttpGet request = new HttpGet(uri);
         
-        String tempCity;
-        String tempState;
-        // TODO - Add dependency injection and mock CloseableHttpClient and CloseableHttpResponse
-        // so that we can test what happens should the lookup service fail
-
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            CloseableHttpResponse response = httpclient.execute(request);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-              	BufferedReader rd = new BufferedReader(
-                        new InputStreamReader(response.getEntity().getContent()));
-           		StringBuilder result = new StringBuilder();
-           		String line = "";
-           		while ((line = rd.readLine()) != null) {
-           			result.append(line);
-       		    }
-                int metaOffset = result.indexOf("<meta ");
-                int contentOffset = result.indexOf(" content=\"Zip Code ", metaOffset);
-                contentOffset += 19;
-                contentOffset = result.indexOf(" - ", contentOffset);
-                contentOffset += 3;
-                int stateOffset = result.indexOf(" ", contentOffset);
-                tempCity = result.substring(contentOffset, stateOffset);
-                stateOffset += 1;
-                tempState = result.substring(stateOffset, stateOffset+2);
-            } else {
-            	tempCity = ""; // TODO - Not covered by a test - see above
-            	tempState = "";
-            }
-            this.city = tempCity;
-            this.state = tempState;
+		CityStateLookup lookup = new CityStateLookup();
+        
+		lookup.lookupCityAndState(zipCode);
+        this.city = lookup.getCity();
+        this.state = lookup.getState();	
         }
-	}
 
 	public String getCity() {
 		return city;
