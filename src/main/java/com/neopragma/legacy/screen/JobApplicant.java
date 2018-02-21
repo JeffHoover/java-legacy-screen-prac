71bccdfb2cfb444858636c7c3aa14a2cf0dcb1d2
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-20 22:22:50 -0500

    Organize imports

--- src/main/java/com/neopragma/legacy/screen/JobApplicant.java
+++ src/main/java/com/neopragma/legacy/screen/JobApplicant.java
@@ -1,19 +1,8 @@
 package com.neopragma.legacy.screen;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Scanner;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 import com.neopragma.legacy.CityStateLookup;
 import com.neopragma.legacy.integration.PersistenceLayer;
