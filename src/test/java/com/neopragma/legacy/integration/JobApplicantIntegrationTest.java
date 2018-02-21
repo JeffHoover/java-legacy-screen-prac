1666412ab140a8af6d5a5e01e184624daa038802
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-20 21:49:56 -0500

    Fixed mistake of test class with wrong name.

--- src/test/java/com/neopragma/legacy/integration/JobApplicantIntegrationTest.java
+++ src/test/java/com/neopragma/legacy/integration/JobApplicantIntegrationTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 import com.neopragma.legacy.screen.JobApplicant;
 
-public class JobApplicantTest {
+public class JobApplicantIntegrationTest {
 
 	private JobApplicant jobApplicant;
 	
