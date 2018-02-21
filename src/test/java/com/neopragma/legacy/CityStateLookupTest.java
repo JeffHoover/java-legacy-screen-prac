07b83c3d5e6c19557ddd3cf100dd95c6c2a083ee
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-20 22:25:49 -0500

    Add test to extracted class.

--- src/test/java/com/neopragma/legacy/CityStateLookupTest.java
+++ src/test/java/com/neopragma/legacy/CityStateLookupTest.java
@@ -1,5 +1,16 @@
 package com.neopragma.legacy;
 
-public class CityStateLookupTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class CityStateLookupTest {
+	@Test
+	public void lookupCityState() throws Exception {
+		CityStateLookup cityStateLookup = new CityStateLookup();
+		cityStateLookup.lookupCityState("02134");
+		
+		assertEquals("Allston", cityStateLookup.getCity());
+		assertEquals("MA", cityStateLookup.getState());
+	}
 }
