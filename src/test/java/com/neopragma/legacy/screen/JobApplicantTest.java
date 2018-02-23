1dcdf84ddf3b7ffa53a22364c76e6687480f4766
Jeff Hoover <jhoover@pillartechnology.com>
2018-02-23 11:56:52 -0500

    Move tests

--- src/test/java/com/neopragma/legacy/screen/JobApplicantTest.java
+++ src/test/java/com/neopragma/legacy/screen/JobApplicantTest.java
@@ -1,10 +1,8 @@
 package com.neopragma.legacy.screen;
 
-import static com.neopragma.legacy.screen.ErrorCode.*;
+import static com.neopragma.legacy.screen.ErrorCode.INVALID_NAME;
+import static com.neopragma.legacy.screen.ErrorCode.SUCCESS;
 import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,24 +14,6 @@ public class JobApplicantTest {
 	@Before
 	public void beforeEach() {
 		jobApplicant = new JobApplicant();
-	}
-	
-	@Test
-	public void addsApplicantName() throws Exception {
-		jobApplicant.add("Jeff", "", "Hoover", "123456789", "02134");
-		assertEquals(SUCCESS, jobApplicant.validateName());
-	}
-	
-	@Test
-	public void addsSSN() throws Exception {
-		jobApplicant.add("Jeff", "", "Hoover", "123456789", "02134");
-		assertEquals(SUCCESS, jobApplicant.validateSsn());
-	}
-	
-	@Test
-	public void addsZipCode() throws Exception {
-		jobApplicant.add("Jeff", "", "Hoover", "123456789", "02134");
-		assertEquals("Allston", jobApplicant.getCity());
 	}
 	
 	@Test
@@ -94,14 +74,6 @@ public class JobApplicantTest {
 	public void formatEnglishNameLastNameFirst() {
 		jobApplicant.setName("First", "Middle", "Last");
 		assertEquals("Last, First Middle", jobApplicant.formatLastNameFirst());
-	}
-
-	@Test
-	public void itCurrentlyGivesWeirdResultForInvalidZip() throws URISyntaxException, IOException {
-		String invalidZipCode = "1";
-		jobApplicant.add("Jeff", "", "Hoover", "123456789", invalidZipCode);
-		assertEquals("Facts", jobApplicant.getCity());
-		assertEquals("& ", jobApplicant.getState());
 	}
 
 }
--- src/test/java/com/neopragma/legacy/screen/integration/JobApplicantIntegrationTest.java
+++ src/test/java/com/neopragma/legacy/screen/integration/JobApplicantIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.neopragma.legacy.screen.integration;
 
+import static com.neopragma.legacy.screen.ErrorCode.SUCCESS;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
@@ -18,18 +19,44 @@ public class JobApplicantIntegrationTest {
 	public void beforeEach() {
 		jobApplicant = new JobApplicant();
 	}
-	
+
 	@Test
 	public void itFindsAddisonTexasBy5DigitZipCode() throws URISyntaxException, IOException {
 		jobApplicant.lookupCityState("75001");
 		assertEquals("Addison", jobApplicant.getCity());
 		assertEquals("TX", jobApplicant.getState());
 	}
-	
+
 	@Test
 	public void itFindsMaranaArizonaBy9DigitZipCode() throws URISyntaxException, IOException {
 		jobApplicant.lookupCityState("856585578");
 		assertEquals("Marana", jobApplicant.getCity());
 		assertEquals("AZ", jobApplicant.getState());
+	}
+
+	@Test
+	public void addsApplicantName() throws Exception {
+		jobApplicant.add("Jeff", "", "Hoover", "123456789", "02134");
+		assertEquals(SUCCESS, jobApplicant.validateName());
+	}
+
+	@Test
+	public void addsSSN() throws Exception {
+		jobApplicant.add("Jeff", "", "Hoover", "123456789", "02134");
+		assertEquals(SUCCESS, jobApplicant.validateSsn());
+	}
+
+	@Test
+	public void addsZipCode() throws Exception {
+		jobApplicant.add("Jeff", "", "Hoover", "123456789", "02134");
+		assertEquals("Allston", jobApplicant.getCity());
+	}
+
+	@Test
+	public void itCurrentlyGivesWeirdResultForInvalidZip() throws URISyntaxException, IOException {
+		String invalidZipCode = "1";
+		jobApplicant.add("Jeff", "", "Hoover", "123456789", invalidZipCode);
+		assertEquals("Facts", jobApplicant.getCity());
+		assertEquals("& ", jobApplicant.getState());
 	}
 }
